7e120603875c3cf66ffc6491375e63001efa7c99
ariaProgrammerFlutter <87ariyaahady@gmail.com>
2023-10-01 18:14:29 +0330

    change body style

--- lib/ui/widget/myTextStyle.dart
+++ lib/ui/widget/myTextStyle.dart
@@ -1,22 +1,16 @@
-
-
 import 'package:flutter/material.dart';
-
-
 
 TextStyle DrawerStyle() {
   return TextStyle(fontSize: 18);
 }
 
+TextStyle outhersStyle() {
+  return TextStyle(fontSize: 20);
+}
 
-
-  TextStyle outhersStyle() => TextStyle(fontSize: 20);
-
-
-  TextStyle allStyle() {
-    return TextStyle(
-      fontSize: 23,
-      color: Colors.black,
-    );
-  }
-
+TextStyle allStyle() {
+  return TextStyle(
+    fontSize: 23,
+    color: Colors.black,
+  );
+}
